c92fb7a6f08b2c2137f2f70cec6314e2f70cd1d3
sunilnayak7045 <sunilnayak7045@gmail.com>
2022-08-08 16:05:53 +0530

    testing

--- app/src/main/java/com/example/sql/MainActivity.kt
+++ app/src/main/java/com/example/sql/MainActivity.kt
@@ -9,6 +9,7 @@ import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.register.*
 
+//testtttt
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
